5eb35fa3e4187912bf63b1b91dc5fd84a8b7fba1
edu-lopes <eduardolbs07@gmail.com>
2026-01-28 15:06:21 -0300

    feat: consulta de cidade, região e microregião via API do IBGE

---
+++ utility_belt/eduardo/ibge/ibge.js
@@ -0,0 +1,17 @@
+async function buscarMunicipios(uf) {
+    try {
+        const resposta = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`);
+        const dados = await resposta.json();
+        return dados
+            .filter(cidade => cidade.nome.startsWith('E'))
+            .map(cidade => ({
+                    nome: cidade.nome,
+                    regiao: cidade['regiao-imediata']?.nome,
+                    microregiao: cidade.microrregiao?.nome
+            }));
+    } catch (error) {
+        console.error("Código não executado! \n Erro detecado:", error.message)
+        return [];
+    }
+}
+buscarMunicipios('PR').then(resultado => console.log(resultado));
