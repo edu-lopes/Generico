0f7c474e822576b6bfbb5261fc8e66ab8a5de14b
edu-lopes <eduardolbs07@gmail.com>
2026-03-12 09:22:46 -0300

    feat: código para revelar uma foto capturada pela Nasa passando um dia como parâmetro

---
+++ utility_belt/eduardo/nasa/nasa.js
@@ -0,0 +1,30 @@
+// Arrow function => Tratamento e requisição de dados (lógica e callbacks)
+const buscarFotoNasa = async (data) => {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${data}`);
+    const info = await response.json();
+    
+    return {
+        titulo: info.title,
+        link: info.url
+    };
+};
+buscarFotoNasa('2023-12-25').then(res => console.log(res));
+
+// Método --> Função dentro de objeto
+const explorador = {
+    nome: "Eduardo",
+    dataDescoberta: "2024-01-01",
+
+    apresentarDescoberta: async function() {
+        console.log(`Olá, eu sou o ${this.nome} e vou buscar a foto de ${this.dataDescoberta}...`);
+        
+        try {
+            const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.dataDescoberta}`);
+            const data = await response.json();
+            console.log(`Sucesso! Título da descoberta: ${data.title}`);
+        } catch (e) {
+            console.log("Erro na missão!");
+        }
+    }
+};
+explorador.apresentarDescoberta();
